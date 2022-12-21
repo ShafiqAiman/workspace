7aa506f7a950160b041060d2c366a13539173d34
Shahrul Nizam <SHAFIQSHAHRUL@kpmg.com.my>
2022-12-21 11:18:56 +0800

    Added Validation on Booking Form

--- roombooking/forms.py
+++ roombooking/forms.py
@@ -1,16 +1,35 @@
 from django import forms
+
+from .models import Room, StartTime, EndTime
 import datetime
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-class RoomAvailabilitiesForm(forms.Form):
-    start = forms.DateField()
+class BookingForm(forms.Form):
+    startdate = forms.DateField(widget=forms.DateInput(attrs={'class':'form-control', 'type':'date'}))
+    enddate = forms.DateField(widget=forms.DateInput(attrs={'class':'form-control', 'type':'date'}))
+    starttime = forms.ModelChoiceField(queryset=StartTime.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
+    endtime = forms.ModelChoiceField(queryset=EndTime.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
+    room = forms.ModelChoiceField(queryset=Room.objects.all(), widget= forms.Select(attrs={'class': 'form-control'}))
+   
+    def clean(self):
+        cleaned_data = super().clean()
+        startdate = cleaned_data.get("startdate")
+        enddate = cleaned_data.get("enddate")
+        starttime = cleaned_data.get("starttime")
+        endtime = cleaned_data.get("endtime")
+        print(type(starttime.time))
+        
+        if startdate < datetime.date.today() or enddate < datetime.date.today():
+            print('Error with Date')
+            raise ValidationError(_('Invalid date - date is in past'))
 
-    def clean_start(self):
-        data = self.cleaned_data['start']
+        if starttime.time >  endtime.time:
+            print('Error with Time')
+            raise ValidationError(_('Invalid time - time is in past'))
 
-        if data < datetime.date.today():
-            print('Error with Start Date')
-            raise ValidationError(_('Invalid date - renewal in past'))
-        return data
+        return cleaned_data
+
+
+        
--- roombooking/views.py
+++ roombooking/views.py
@@ -3,11 +3,13 @@ from django.views import generic
 from .models import Room, Booking
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.views.generic import FormView
+from django.urls import reverse_lazy, reverse
 from django.contrib.admin.widgets import AdminDateWidget, AdminRadioSelect
+from django.shortcuts import HttpResponse, HttpResponseRedirect
 
 import datetime
-from roombooking.forms import RoomAvailabilitiesForm
+from roombooking.forms import BookingForm
 
 # Create your views here.
 def index(request):
@@ -15,32 +17,14 @@ def index(request):
     return render(request, 'index.html')
 
 def roomavailabilities(request):
-    rooms = Room.objects.all
-    bookings = Booking.objects.all
+    rooms = Room.objects.all()
+    bookings = Booking.objects.all()
     today = datetime.date.today()
     start = today
     if request.method == 'POST':
-        print("The request is POST")
-        form = RoomAvailabilitiesForm(request.POST)
-
-        if form.is_valid():
-            start = form.cleaned_data['start']
-            print("Valid date")
+        start = request.POST["start"]
         
     bookings = Booking.objects.filter(startdate = start)
-
-    # roomsfilter = Room.objects
-
-    roomlist = []
-    # timelist = []
-    
-
-    for booking in bookings:
-        roomlist.append(booking.room)
-        # timelist.append([booking.starttime, booking.endtime])
-        print(roomlist)
-    #     print(timelist)
-
 
     # rooms = roomsfilter.exclude(name__in = roomlist)
             
@@ -91,25 +75,26 @@ class RoomDelete(DeleteView):
     model = Room
     success_url = reverse_lazy('Rooms')
 
-class BookingCreate(CreateView):
+class BookingCreate(FormView):
     model = Booking
-    fields = ['startdate','starttime', 'enddate', 'endtime','room', 'organizer']
+    form_class = BookingForm
+    template_name = 'roombooking/create_booking.html'
     initial = {'startdate': datetime.date.today(), 'enddate': datetime.date.today()}
 
-    def get_form(self, form_class=None):
-        form = super(BookingCreate, self).get_form(form_class)
-        form.fields['startdate'].widget = AdminDateWidget(attrs={'type': 'date'})
-        # form.fields['starttime'].widget = AdminDateWidget(attrs={'type': 'time'})
-        form.fields['enddate'].widget = AdminDateWidget(attrs={'type': 'date'})
-        # form.fields['endtime'].widget = AdminDateWidget(attrs={'type': 'time'})
-        form.fields['startdate'].widget.attrs.update({'class': 'form-control'})
-        form.fields['starttime'].widget.attrs.update({'class': 'form-control'})
-        form.fields['enddate'].widget.attrs.update({'class': 'form-control'})
-        form.fields['endtime'].widget.attrs.update({'class': 'form-control'})
-        form.fields['room'].widget.attrs.update({'class': 'form-control'})
-        form.fields['organizer'].widget.attrs.update({'class': 'form-control'})
-        return form
+    def form_valid(self, form):
+        data = form.cleaned_data
+        booking = Booking.objects.create(
+            startdate = data['startdate'],
+            enddate = data['enddate'],
+            starttime = data['starttime'],
+            endtime = data['endtime'],
+            room = data['room'],
+            organizer = self.request.user
+        )
+        booking.save()
+        return HttpResponseRedirect(reverse('bookings'))
 
+    
 class BookingUpdate(UpdateView):
     model = Booking
     fields = '__all__' # Not recommended (potential security issue if more fields added)
@@ -117,9 +102,9 @@ class BookingUpdate(UpdateView):
     def get_form(self, form_class=None):
         form = super(BookingUpdate, self).get_form(form_class)
         form.fields['startdate'].widget = AdminDateWidget(attrs={'type': 'date'})
-        # form.fields['starttime'].widget = AdminDateWidget(attrs={'type': 'time'})
+        
         form.fields['enddate'].widget = AdminDateWidget(attrs={'type': 'date'})
-        # form.fields['endtime'].widget = AdminDateWidget(attrs={'type': 'time'})
+        
         form.fields['startdate'].widget.attrs.update({'class': 'form-control'})
         form.fields['starttime'].widget.attrs.update({'class': 'form-control'})
         form.fields['enddate'].widget.attrs.update({'class': 'form-control'})
